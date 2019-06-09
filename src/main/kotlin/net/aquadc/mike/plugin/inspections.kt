bad2d1b8a2fc0b548511140055356898a877001d
Miha-x64 <silizium@ok.de>
2019-06-09 14:51:49 +0300

    Init with property delegation inspection

---
+++ src/main/kotlin/net/aquadc/mike/plugin/inspections.kt
@@ -0,0 +1,18 @@
+package net.aquadc.mike.plugin
+
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
+import org.jetbrains.kotlin.psi.propertyVisitor
+
+
+class KtPropByInspection : AbstractKotlinInspection() {
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor =
+        propertyVisitor { prop ->
+            prop.delegate?.let { delegate ->
+                holder.registerProblem(delegate, "Property delegation")
+            }
+        }
+
+}
