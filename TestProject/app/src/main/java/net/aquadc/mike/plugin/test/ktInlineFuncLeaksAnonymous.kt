c7406f81379cfa96d32b338e97be438d414c7e80
Miha_x64 <mihlo1996@rambler.ru>
2020-03-20 20:27:16 +0300

    shut up false-positive KtInlineFunctionLeaksAnonymousDeclaration

--- TestProject/app/src/main/java/net/aquadc/mike/plugin/test/ktInlineFuncLeaksAnonymous.kt
+++ TestProject/app/src/main/java/net/aquadc/mike/plugin/test/ktInlineFuncLeaksAnonymous.kt
@@ -2,12 +2,33 @@
 package net.aquadc.mike.plugin.test
 
 
-inline fun leaks(zzz: () -> Unit) {
+inline fun leak1(zzz: () -> Unit) {
     val a = { }
-    val b = ::dontLeakMe
+    val b = ::whatever
 }
 
-fun dontLeakMe(zzz: () -> Unit) {
-    val a = { }
-    val b = ::dontLeakMe
+inline fun leak2(crossinline zzz: () -> Unit) {
+    whatever { } // TODO
 }
+
+
+inline fun noLeak1(crossinline zzz: () -> Unit) {
+    whatever { zzz() }
+}
+
+inline fun noLeak2(crossinline zzz: () -> Unit) =
+    Runnable { zzz() }
+
+inline fun noLeak3(crossinline zzz: () -> Unit) =
+    object : Runnable {
+        override fun run() {
+            zzz()
+        }
+    }
+
+fun noLeak() {
+    val a = { }
+    val b = ::whatever
+}
+
+fun whatever(zzz: () -> Unit) = Unit
--- src/main/kotlin/net/aquadc/mike/plugin/kotlin/KtInlineFunctionLeaksAnonymousDeclaration.kt
+++ src/main/kotlin/net/aquadc/mike/plugin/kotlin/KtInlineFunctionLeaksAnonymousDeclaration.kt
@@ -7,6 +7,7 @@ import net.aquadc.mike.plugin.containingFunction
 import net.aquadc.mike.plugin.isInline
 import net.aquadc.mike.plugin.noinlineMessage
 import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
+import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtFunction
 import org.jetbrains.kotlin.psi.psiUtil.isPublic
@@ -21,6 +22,7 @@ class KtInlineFunctionLeaksAnonymousDeclaration : AbstractKotlinInspection() {
                     ?.takeIf(KtFunction::isInline)
                     ?.takeIf(KtFunction::isPublic)
                     ?.takeIf { noinlineMessage(expression) != null }
+                    ?.takeIf { !expression.containsCrossinlinesFrom(it) }
                     ?.let {
                         holder.registerProblem(expression,
                             "This anonymous declaration will be copied to the call-site of enclosing inline function if called from another module"
@@ -28,6 +30,13 @@ class KtInlineFunctionLeaksAnonymousDeclaration : AbstractKotlinInspection() {
                     }
             }
 
-        }
+            private fun KtExpression.containsCrossinlinesFrom(func: KtFunction): Boolean {
+                val xInlines = func.valueParameters.filter { it.hasModifier(KtTokens.CROSSINLINE_KEYWORD) }
+                if (xInlines.isEmpty()) return false
 
+                // todo
+
+                return true
+            }
+        }
 }
