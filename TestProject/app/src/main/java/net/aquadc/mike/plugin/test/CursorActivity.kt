1c78404736d76f5cca75a8e819366e2f33faf9f7
Miha-x64 <mihlo1996@rambler.ru>
2022-05-28 21:25:11 +0300

    oops... addition to layer/inset/shape showcase

---
+++ TestProject/app/src/main/java/net/aquadc/mike/plugin/test/CursorActivity.kt
@@ -0,0 +1,11 @@
+package net.aquadc.mike.plugin.test
+
+import android.app.Activity
+import android.os.Bundle
+
+class CursorActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cursor)
+    }
+}
