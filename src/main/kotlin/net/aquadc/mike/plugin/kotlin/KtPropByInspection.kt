01e3db02daecf58ef35b64dd2fedea2ceb4740a7
Miha-x64 <mihlo1996@rambler.ru>
2023-07-15 00:42:56 +0300

    unuse incompatible KtDescriptorsBasedReference

--- src/main/kotlin/net/aquadc/mike/plugin/kotlin/KtPropByInspection.kt
+++ src/main/kotlin/net/aquadc/mike/plugin/kotlin/KtPropByInspection.kt
@@ -4,15 +4,20 @@ import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElementVisitor
+import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+import org.jetbrains.kotlin.descriptors.VariableDescriptorWithAccessors
+import org.jetbrains.kotlin.descriptors.accessors
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.idea.quickfix.AddModifierFix
-import org.jetbrains.kotlin.idea.references.KtDescriptorsBasedReference
 import org.jetbrains.kotlin.idea.search.isPotentiallyOperator
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.KtPropertyDelegate
 import org.jetbrains.kotlin.psi.KtVisitorVoid
+import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
+import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.util.OperatorNameConventions
 
 /**
@@ -25,14 +30,20 @@ class KtPropByInspection : LocalInspectionTool() {
     ): PsiElementVisitor = object : KtVisitorVoid() {
         override fun visitProperty(property: KtProperty) {
             property.delegate?.let { delegate ->
-                if (delegate.references.any {
-                    (it as? KtDescriptorsBasedReference)?.getTargetDescriptors(property.analyze())?.any {
-                        (it as? FunctionDescriptor)?.isInline == false
-                    } == true
-                }) {
+                if (delegate.getTargetDescriptors(property.analyze())
+                    .any { (it as? FunctionDescriptor)?.isInline == false }) {
                     holder.registerProblem(delegate, TextRange.from(0, 2), "Heavyweight property delegation")
                 }
             }
+        }
+        // copy-paste from hidden org.jetbrains.kotlin.idea.references.KtPropertyDelegationMethodsReferenceDescriptorsImpl.getTargetDescriptors
+        fun KtPropertyDelegate.getTargetDescriptors(context: BindingContext): Collection<DeclarationDescriptor> {
+            val property = expression?.getStrictParentOfType<KtProperty>() ?: return emptyList()
+            val descriptor = context[BindingContext.DECLARATION_TO_DESCRIPTOR, property] as? VariableDescriptorWithAccessors
+                ?: return emptyList()
+            return descriptor.accessors.mapNotNull { accessor ->
+                context.get(BindingContext.DELEGATED_PROPERTY_RESOLVED_CALL, accessor)?.candidateDescriptor
+            } + listOfNotNull(context.get(BindingContext.PROVIDE_DELEGATE_RESOLVED_CALL, descriptor)?.candidateDescriptor)
         }
         override fun visitNamedFunction(function: KtNamedFunction) {
             if (function.isDelegationFunction && !function.hasModifier(KtTokens.INLINE_KEYWORD)) {
