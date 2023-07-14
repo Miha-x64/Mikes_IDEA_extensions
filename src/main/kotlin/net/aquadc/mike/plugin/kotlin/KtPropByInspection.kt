b332f963a37b38ff1e61e66d58290b32da9795cf
Miha-x64 <mihlo1996@rambler.ru>
2023-07-15 00:44:41 +0300

    unuse incompatible org.jetbrains.kotlin.idea.search.isPotentiallyOperator

--- src/main/kotlin/net/aquadc/mike/plugin/kotlin/KtPropByInspection.kt
+++ src/main/kotlin/net/aquadc/mike/plugin/kotlin/KtPropByInspection.kt
@@ -3,6 +3,7 @@ package net.aquadc.mike.plugin.kotlin
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
@@ -10,15 +11,17 @@ import org.jetbrains.kotlin.descriptors.VariableDescriptorWithAccessors
 import org.jetbrains.kotlin.descriptors.accessors
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.idea.quickfix.AddModifierFix
-import org.jetbrains.kotlin.idea.search.isPotentiallyOperator
 import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.KtPropertyDelegate
 import org.jetbrains.kotlin.psi.KtVisitorVoid
 import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
 import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.types.expressions.OperatorConventions
 import org.jetbrains.kotlin.util.OperatorNameConventions
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 /**
  * @author Mike Gorünóv
@@ -56,11 +59,23 @@ class KtPropByInspection : LocalInspectionTool() {
         }
 
         private val KtNamedFunction.isDelegationFunction: Boolean
-            get() = isPotentiallyOperator() && name?.let {
+            get() = isPotentiallyOperator && name?.let {
                 it == OperatorNameConventions.PROVIDE_DELEGATE.asString() ||
                         it == OperatorNameConventions.GET_VALUE.asString() ||
                         it == OperatorNameConventions.SET_VALUE.asString()
             } ?: false
+
+        // org.jetbrains.kotlin.idea.search.isPotentiallyOperator
+        val PsiElement?.isPotentiallyOperator: Boolean
+            get() {
+                val namedFunction = safeAs<KtNamedFunction>() ?: return false
+                if (namedFunction.hasModifier(KtTokens.OPERATOR_KEYWORD)) return true
+                // operator modifier could be omitted for overriding function
+                if (!namedFunction.hasModifier(KtTokens.OVERRIDE_KEYWORD)) return false
+                val name = namedFunction.name ?: return false
+                if (!OperatorConventions.isConventionName(Name.identifier(name))) return false
+                return true
+            }
     }
 
 }
